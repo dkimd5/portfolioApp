9590e513d70f38975a750484e6fdadf6ebe65f38
dkimd5 <dkimd5@mail.ru>
2021-11-21 12:53:01 +0300

    sections title and text added

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,17 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tristique aliquam est, a ullamcorper nisi interdum quis. Etiam pulvinar eleifend odio, in mollis arcu tempor.
+      </SectionText>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
