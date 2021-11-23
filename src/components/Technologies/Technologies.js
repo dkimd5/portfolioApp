2ce585188087eec5263210b7da598c5cf709e56d
dkimd5 <dkimd5@mail.ru>
2021-11-23 11:01:50 +0300

    title and text added

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -4,9 +4,13 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla laoreet, nunc ut ornare rhoncus, lacus nibh cursus.
+    </SectionText>
+  </Section>
 );
 
 export default Technologies;
